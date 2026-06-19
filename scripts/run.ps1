a91fce3731afee192344aa8ff0072e7c845b2b20
Jason Holdsworth <jason.holdsworth@jcu.edu.au>
2026-06-19 20:21:11 +1000

    Use Python config loader in PowerShell launch script

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,10 +1,8 @@
 $ErrorActionPreference = "Stop"
 
-$HostName = if ($env:TOKEN_TRAIL_HOST) { $env:TOKEN_TRAIL_HOST } else { "127.0.0.1" }
-$Port = if ($env:TOKEN_TRAIL_PORT) { $env:TOKEN_TRAIL_PORT } else { "3100" }
-
 $env:PYTHONPATH = "src"
-poetry run python -m token_trail.ports --host $HostName --port $Port
 
-Write-Host "Starting Token Trail at http://${HostName}:${Port} ..."
-poetry run python -m token_trail.server --host $HostName --port $Port
+poetry run python -m token_trail.ports
+
+Write-Host "Starting Token Trail using .env/default configuration..."
+poetry run python -m token_trail.server
